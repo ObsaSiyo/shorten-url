695ad3436c1bfd459ac74425c64b5ad7eda345ee
Obsa Siyo <obsasiyo147@gmail.com>
2026-05-07 19:33:23 -0500

    index mono

--- constants.js
+++
@@ -1,3 +0,0 @@
-URL_REGEX=/https?:\/\/(?!(?:[\w-]+\.)*(?:cdn\.discordapp\.com|media\.discordapp\.net|tenor\.com|giphy\.com|c\.tenor\.com|i\.giphy\.com)(?:\/|\s|$))(?:[\w-]+\.)+[\w-]+(?:\/[^\s<>"']*)?/gi
-const MIN_LENGTH = 23;
-module.exports = { URL_REGEX, MIN_LENGTH };
--- utils/urlShortener.js
+++
@@ -1,7 +0,0 @@
-async function shortenUrl(url) {
-  const res = await fetch(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(url)}`);
-  if (!res.ok) return url;
-  return (await res.text()).trim();
-}
-
-module.exports = { shortenUrl };
